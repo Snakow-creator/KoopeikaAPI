2cffb63baa3267a1ba1fe36bb4cc3f9661905318
Snakow-creator <mark.politov20070@gmail.com>
2025-09-14 22:44:39 +0300

    v0.2.1 fix

--- src/main.py
+++ src/main.py
@@ -1,26 +1,40 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from apscheduler import AsyncScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
+
+from contextlib import asynccontextmanager
 
 from api.__init__ import init_router
-from schedulers.__init__ import start_scheduler
 
-import asyncio
+from rates.upload_rates import upload_all_rates, upload_rates, upload_top_cryptorates
+
 import uvicorn
 
-app = FastAPI()
+
+@asynccontextmanager
+async def main(app: FastAPI):
+    async with AsyncScheduler() as scheduler:
+        # actions on startup
+        await upload_all_rates()
+
+        await scheduler.start_in_background()
+        await scheduler.add_schedule(upload_rates, CronTrigger(hour=0, minute=0), id="upload_rates")
+        await scheduler.add_schedule(upload_top_cryptorates, IntervalTrigger(minutes=5), id="upload_top_cryptorates")
+        yield
+        # actions on shutdown
+        print("KoopeikaAPI is stopped")
 
 
-async def main():
-    await start_scheduler()
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
+app = FastAPI(lifespan=main)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
     )
-    app.include_router(init_router)
-
+app.include_router(init_router)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
